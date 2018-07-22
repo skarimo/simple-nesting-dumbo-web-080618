957815b29ba2d30a94a7945a4495f6a17c599c54
Sherzod Karimov <karimovj@gmail.com>
2018-07-22 20:44:44 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -129,7 +129,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  arr = programmer_hash[:dennis_ritchie][:languages]
-  arr << "Assembly"
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
   programmer_hash
 end
